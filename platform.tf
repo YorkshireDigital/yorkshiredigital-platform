5e01dbad8e044140eb0c98d2a148d23dcfb55209
MacsDickinson <macs.dickinson@live.co.uk>
2016-07-24 10:50:41 +0100

    added terraform files

---
+++ platform.tf
@@ -0,0 +1,58 @@
+resource "brightbox_cloudip" "webip" {
+  target = "${brightbox_server.webserver.interface}"
+  name = "web ip"
+}
+
+resource "brightbox_server" "webserver" {
+  image = "${var.image_id}"
+  name = "web server"
+  server_groups = ["${brightbox_server_group.webservers.id}"]
+  depends_on = ["brightbox_firewall_policy.webservers"]
+}
+
+resource "brightbox_server_group" "webservers" {
+  name = "web servers"
+}
+
+
+resource "brightbox_cloudip" "db" {
+  target = "${brightbox_database_server.db.id}"
+  name = "db ip"
+}
+
+resource "brightbox_database_server" "db" {
+  name = "db server"
+  database_engine = "mysql"
+  database_version = "5.6"
+  maintenance_weekday = 6
+  maintenance_hour = 2
+  allow_access = ["${brightbox_server_group.webservers.id}"]
+}
+
+
+resource "brightbox_firewall_policy" "webservers" {
+  name = "web servers"
+  server_group = "${brightbox_server_group.webservers.id}"
+}
+
+resource "brightbox_firewall_rule" "webservers_ssh" {
+    destination_port = 22
+    protocol = "tcp"
+    source = "any"
+    description = "SSH access from anywhere"
+    firewall_policy = "${brightbox_firewall_policy.webservers.id}"
+}
+
+resource "brightbox_firewall_rule" "webservers_web" {
+    destination_port = "80,443"
+    protocol = "tcp"
+    source = "any"
+    description = "HTTP/S access from anywhere"
+    firewall_policy = "${brightbox_firewall_policy.webservers.id}"
+}
+
+resource "brightbox_firewall_rule" "webservers_outbound" {
+    destination = "any"
+    description = "Outbound internet access"
+    firewall_policy = "${brightbox_firewall_policy.webservers.id}"
+}
